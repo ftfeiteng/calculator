8756846fe7ec926fd833bc07eeea2ffba4e9b4d7
Tengf <ftfeiteng@gmail.com>
2018-11-09 11:49:02 +1100

    Add user interaction.

--- src/com/example/main/Main.java
+++ src/com/example/main/Main.java
@@ -1,9 +1,31 @@
 package com.example.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Calculator\nPlease input your text:");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String text = null;
+		try {
+			text = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Calculator cal = new Calculator();
+		try {
+			int ret = cal.calculate(text);
+			System.out.println("" + ret);
+		} catch (SyntaxException e) {
+			System.out.println("Error: Syntax Error");
+		} catch (DividByZeroException e) {
+			System.out.println("Error: Division by zero");
+		}
 
 	}
 
